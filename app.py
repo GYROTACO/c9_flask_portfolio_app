f8d75d051ef01a806786f9db2f22c41433812c2e
GYROTACO <94348337+GYROTACO@users.noreply.github.com>
2021-11-15 20:04:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	return render_template('index.html', name=name)
+	new_name = name + "likes to eat mangos"
+	return render_template('index.html', name=new_ame)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
